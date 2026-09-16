49da8891f5013ebb4c9233f8e6559231953ec74d
thehazell <taco.xr@proton.me>
2026-09-15 23:02:03 -0500

    fix: normalize resource names from the instance name var

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,4 +1,4 @@
 resource "cloudflare_r2_bucket" "profile" {
-	account_id = var.cloudflare_account_id
-	name       = local.profile_bucket_name
+  account_id = var.cloudflare_account_id
+  name       = local.profile_bucket_name
 }
